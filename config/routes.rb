e571e5da2c685a41e33810934b267757eb1158ae
SeijiNumata <seizi.numata@gmail.com>
2021-06-23 01:23:46 +0900

    vueを使えるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'top#index'
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
