effe1f7bf66e00b7772188745e99b590177a742b
SeijiNumata <seizi.numata@gmail.com>
2021-06-27 12:46:19 +0900

    routeを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'top#index'
+  # resources :items
   resources :events
+  namespace :api do # , format: 'json' do
+    resources :items # , only: [:index, :create, :destroy, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
