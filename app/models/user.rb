6829e0530af7c032fc422cf1a98f401f31679476
SeijiNumata <seizi.numata@gmail.com>
2021-06-30 12:52:32 +0900

    アソシエーションをモデルに記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :users_events
     has_many :events, through: :users_events
     accepts_nested_attributes_for :users_events
-
+    has_many :user_bring_items
+    has_many :items, through: :user_bring_items
 end
--- app/models/user_bring_item.rb
+++ app/models/user_bring_item.rb
@@ -1,2 +1,4 @@
 class UserBringItem < ApplicationRecord
+    belongs_to :item
+    belongs_to :user
 end
