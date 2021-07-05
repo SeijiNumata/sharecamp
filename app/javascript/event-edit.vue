cb14ec01f2894a1eda13d93e8d0263f15ab67c0d
SeijiNumata <seizi.numata@gmail.com>
2021-07-05 15:21:46 +0900

    editのページを追加

--- app/javascript/event-edit.vue
+++ app/javascript/event-edit.vue
@@ -1,37 +1,38 @@
 <template>
-<div>
-<p>{{items}}</p>
-  <form>
- <ul>
+  <div>
+    <p>{{items}}</p>
+    <form>
+      <ul>
         <li v-for="(neededItemInfo, index) in neededItemInfos" :key="neededItemInfo.id">
           <!-- 各入力ボックス -->
-          <input v-model="neededItemInfo.name" :readonly="neededItemInfo.readonly" :class="{readonly:neededItemInfo.readonly}">
+          <input v-model="neededItemInfo.name" :readonly="neededItemInfo.readonly"
+            :class="{readonly:neededItemInfo.readonly}">
           <input type="number" name="num01" min="0" v-model="neededItemInfos[index].need_number">
-          
+
         </li>
-         
+
       </ul>
       <button type="button" @click="addInput">追加する</button>
       <button type="button" @click="updateItems">この内容で登録</button>
     </form>
-    </div>
+  </div>
 </template>
 
 <script>
- import axios from 'axios';
+  import axios from 'axios';
   import eventShowItem from './event-show-item.vue'
   import eventShowCurrentUserItem from './event-show-current-user-item.vue'
-  
+
   export default {
     components: {
       "event-show-item": eventShowItem,
       "event-show-current-user-item": eventShowCurrentUserItem
-       },
+    },
     props: {
       currentUserId: {
         type: String
       },
-      pageUrl:{
+      pageUrl: {
         type: String
       }
     },
@@ -45,9 +46,9 @@
         newEventsName: '',
         selectedNumber: "",
         eventEditUrl: "",
-        getItemRequestUrl:"",
-        eventEditUrl:"",
-        neededItemInfos:[]
+        getItemRequestUrl: "",
+        eventEditUrl: "",
+        neededItemInfos: []
       }
     },
     mounted() {
@@ -62,31 +63,36 @@
         this.setItemRequestUrl(url)
         this.setEditUrl(url)
       },
-      setItemRequestUrl(url){
+      setItemRequestUrl(url) {
         const requestEventURLIndexFront = 21 // /events/IDを取得する
         const requestEventUrlIndexBack = 44
-        this.getItemRequestUrl = (url.slice(requestEventURLIndexFront)).slice( 0, requestEventUrlIndexBack )
+        this.getItemRequestUrl = (url.slice(requestEventURLIndexFront)).slice(0, requestEventUrlIndexBack)
 
       },
-      setEditUrl(url){
+      setEditUrl(url) {
         const eventIdIndex = 29
-        const eventId=url.slice(eventIdIndex)
-        this.eventEditUrl=eventId+"/edit"
+        const eventId = url.slice(eventIdIndex)
+        this.eventEditUrl = eventId + "/edit"
       },
       getItems() {
         console.log(this.getItemRequestUrl + ".json")
-        axios.get(this.getItemRequestUrl+ ".json")
+
+        axios.get(this.getItemRequestUrl + ".json")
           .then((response) => {
             console.log("items" + response.data.item)
             this.items = response.data.item
             console.log(response.data)
             console.log(this.items)
-          for (const item of this.items){
-             console.log("----")
-            console.log(item.name+""+item.need_number)
-            this.neededItemInfos.push({name: item.name ,need_number: item.need_number, readonly: true})
-            console.log(this.neededItemInfos)
-           }
+            for (const item of this.items) {
+              console.log("----")
+              console.log(item.name + "" + item.need_number)
+              this.neededItemInfos.push({
+                name: item.name,
+                need_number: item.need_number,
+                readonly: true
+              })
+              console.log(this.neededItemInfos)
+            }
             console.log(this.items)
             this.eventName = response.data.name
           }, (error) => {
@@ -94,23 +100,33 @@
           });
       },
       addInput() {
-        this.neededItemInfos.push({name: " ",need_number: ""}); // 配列に１つ空データを追加する
+        this.neededItemInfos.push({
+          name: " ",
+          need_number: ""
+        }); // 配列に１つ空データを追加する
       },
-      updateItems(){
-        const eventsID=this.getItemRequestUrl.slice(8)
-        console.log("URKRR"+eventsID)
+      updateItems() {
+        const eventsID = this.getItemRequestUrl.slice(8)
+        console.log("URKRR" + eventsID)
         console.log(this.neededItemInfos)
-        const requestPath="/api/events/"+eventsID
-                axios.patch(requestPath,{items: this.neededItemInfos})
-                .then(() => {
-                   
-                }, (error) => {
-                    console.log(error, response);
-                });
+        const requestPath = "/api/events/" + eventsID
+        axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
+        axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content')
+        axios.patch(requestPath, {
+            items: this.neededItemInfos
+          })
+          .then((response) => {
+            const status = JSON.stringify(response.status)
+            console.log(requestPath)
+
+            const showPath = "/events/" + eventsID
+
+            location.href = showPath
+
+          }, (error) => {
+            console.log(error, response);
+          });
       }
     }
   }
-
-
 </script>
-
