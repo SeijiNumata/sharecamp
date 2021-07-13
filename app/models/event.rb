fb5931e67b5613fca997668f9828d3fdde1fe45c
SeijiNumata <seizi.numata@gmail.com>
2021-07-14 03:05:37 +0900

    モデルを修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,6 @@ class Event < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :users_events, dependent: :destroy
   has_many :users, through: :users_events
-  accepts_nested_attributes_for :users_events
 
   before_create :generate_token
 
