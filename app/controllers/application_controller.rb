c8f3a920ea8a78375d3956e1a2125070724126eb
SeijiNumata <seizi.numata@gmail.com>
2021-07-20 20:59:00 +0900

    インデントを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@
 
 class ApplicationController < ActionController::Base
   def current_user
-    if cookies.signed[:user_id].nil?
-      nil
-    else
-      user_id = cookies.signed[:user_id]
-      User.find_by(id: user_id)
-    end
+    user_id = cookies.signed[:user_id]
+    User.find_by(id: user_id)
   end
 end
