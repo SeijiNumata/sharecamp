da1c247eb77653cb464352c3b65f57c56f62252c
SeijiNumata <seizi.numata@gmail.com>
2021-07-20 22:24:34 +0900

    mysqlからpostgresに変更

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,47 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_716_085_345) do
-  create_table 'events', id: :string, force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_07_16_085345) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", id: :string, force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'event_id', null: false
-    t.text 'name', null: false
-    t.integer 'need_number', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "items", force: :cascade do |t|
+    t.string "event_id", null: false
+    t.text "name", null: false
+    t.integer "need_number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'user_bring_items', force: :cascade do |t|
-    t.integer 'item_id', null: false
-    t.integer 'user_id', null: false
-    t.integer 'bring_number', null: false
-    t.boolean 'check', default: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[item_id user_id], name: 'index_user_bring_items_on_item_id_and_user_id', unique: true
+  create_table "user_bring_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "user_id", null: false
+    t.integer "bring_number", null: false
+    t.boolean "check", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id", "user_id"], name: "index_user_bring_items_on_item_id_and_user_id", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users_events', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'user_id', null: false
-    t.string 'event_id', null: false
-    t.index ['event_id'], name: 'index_users_events_on_event_id'
-    t.index %w[user_id event_id], name: 'index_users_events_on_user_id_and_event_id', unique: true
-    t.index ['user_id'], name: 'index_users_events_on_user_id'
+  create_table "users_events", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.string "event_id", null: false
+    t.index ["event_id"], name: "index_users_events_on_event_id"
+    t.index ["user_id", "event_id"], name: "index_users_events_on_user_id_and_event_id", unique: true
+    t.index ["user_id"], name: "index_users_events_on_user_id"
   end
 
-  add_foreign_key 'users_events', 'events'
-  add_foreign_key 'users_events', 'users'
+  add_foreign_key "users_events", "events"
+  add_foreign_key "users_events", "users"
 end
