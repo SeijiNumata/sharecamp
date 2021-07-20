2f2cd01f925e203279c9db1ca619ff4f7a8ed4e4
SeijiNumata <seizi.numata@gmail.com>
2021-07-21 08:44:46 +0900

    migrationできるようにtype:stringをmigrationファイルに追記

--- db/migrate/20210628114315_add_references_to_users_events.rb
+++ db/migrate/20210628114315_add_references_to_users_events.rb
@@ -3,6 +3,6 @@
 class AddReferencesToUsersEvents < ActiveRecord::Migration[6.1]
   def change
     add_reference :users_events, :user, foreign_key: true
-    add_reference :users_events, :event, foreign_key: true
+    add_reference :users_events, :event, foreign_key: true, type: :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_085345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", id: :string, force: :cascade do |t|
+  create_table "events", id: :serial, force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_085345) do
   create_table "users_events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "event_id", null: false
     t.index ["event_id"], name: "index_users_events_on_event_id"
     t.index ["user_id", "event_id"], name: "index_users_events_on_user_id_and_event_id", unique: true
