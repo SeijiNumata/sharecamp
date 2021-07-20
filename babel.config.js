8d6755bfeae401dcb733610eb48a6a9a84fa7b34
SeijiNumata <seizi.numata@gmail.com>
2021-07-20 22:58:55 +0900

    loose:falseにbabelの設定ファイルを変更

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
   var validEnv = ['development', 'test', 'production']
   var currentEnv = api.env()
   var isDevelopmentEnv = api.env('development')
@@ -44,7 +44,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-proposal-class-properties',
         {
-          loose: true
+          loose: false
         }
       ],
       [
