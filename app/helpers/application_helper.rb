96490022652b1400f3797c4a7a47f9c25dc6aac7
SeijiNumata <seizi.numata@gmail.com>
2021-08-05 02:07:33 +0900

    新規作成ページのデザインを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
   def page_heading(title)
     content_for(:title) { title }
   end
+
+  def recent_watch_events_count
+    return unless cookies[:recent_watch_events]
+    
+    JSON.parse(cookies[:recent_watch_events]).count
+  end
+
 end
