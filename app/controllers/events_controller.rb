67531c5705b0b2e77477da49a0870fa24ad36eef
SeijiNumata <seizi.numata@gmail.com>
2021-08-06 21:17:23 +0900

    不要なjsonのresponseを削除

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,25 +24,7 @@ class EventsController < ApplicationController
 
   def edit; end
 
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  def update; end
 
   private
 
